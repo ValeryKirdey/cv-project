576d3badb3d12fe0504ee0a9655219eda8f8b0c0
ValeryKirdey <v.a.l.e.r.a95@mail.ru>
2022-02-28 23:07:14 +0300

    feat: starting make burger menu

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,7 @@
-console.log("hi there")
+var menuBtn = document.querySelector('.menu-btn');
+var menu = document.querySelector('.navbar-menu');
+
+menuBtn.addEventListener('click', function() {
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
