ea92696f46c9317d74da42e134e6c5360c7aafd5
ValeryKirdey <v.a.l.e.r.a95@mail.ru>
2022-03-01 23:04:42 +0300

    feat: create burger menu and add few changes

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,14 @@
-var menuBtn = document.querySelector('.menu-btn');
-var menu = document.querySelector('.navbar-menu');
+var navbarBtn = document.querySelector('#burger');
+var navbarMenu = document.querySelector('.navbar-menu');
 
-menuBtn.addEventListener('click', function() {
-    menuBtn.classList.toggle('active');
-    menu.classList.toggle('active');
-})
+function toggleMenu() {
+    navbarMenu.classList.toggle('active');
+}
+
+function closeNav() {
+    // navbarBtn.classList.toggle('#burger');
+    navbarMenu.classList.remove('active');
+}
+
+navbarMenu.addEventListener('click', closeNav);
+navbarBtn.addEventListener('click', toggleMenu);
